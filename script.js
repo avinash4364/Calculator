e2fc2bc7e7b4e0af545b5e23290e9148c4b2e669
aky <aky25199@gmail.com>
2025-12-20 19:51:28 +0530

    Fix various bugs and add operation for decimal numbers

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ const resultBtn = document.querySelector(".result-btn");
 const clearBtn = document.querySelector(".ac-btn");
 const clearEntryBtn = document.querySelector(".ce-btn");
 const backspaceBtn = document.querySelector(".bs-btn");
-const display = document.querySelector(".cal-display");
-let isDisplayPopulated = true; // global boolean variable to check if the display is populated at the current moment or not
+const decimalBtn = document.querySelector(".decimal-btn");
+const eqnDisplay = document.querySelector(".cal-equation");
+const resultDisplay = document.querySelector(".cal-result");
 
 function add(num1, num2) {
-    return num1 + num2;
+    // round upto 3 places
+    return Math.round((num1 + num2) * 1000) / 1000;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    // round upto 3 places
+    return Math.round((num1 - num2) * 1000) / 1000;
 }
 
 function divide(num1, num2) {
@@ -33,83 +36,122 @@ function operate(num1, num2, operator) {
         result = subtract(num1, num2);
     } else if (operator === "*") {
         result = multiply(num1, num2);
-    } else if (operator === "/") {
+    } else if (operator === "÷") {
         result = divide(num1, num2);
     } else {
-        console.log("Wrong operator");
+        populateDisplay("INVALID OPERATION", resultDisplay);
     }
     return result;
 }
 
 function parseInput(text) {
+    let invalidKeysArray = ["", " ", "  ", "   ", "    "];
     // const regex = /[+-*/]/; there is no valid character range from + to * unlike [a-z]
-    const regex = /([+/*-])/; // () using capturing parenthesis matched results are included in the array , so +,-,*,/ are included in the array
+    const regex = /([*+÷-])/; // () using capturing parenthesis matched results are included in the array , so +,-,*,/ are included in the array
     return text.trim().split(regex);
 }
 
-function clearDisplay() {
-    display.textContent = "";
-}
-
-function clearEntry() {
-    clearDisplay();
-}
-
-function populateDisplay(text) {
-    if (display.textContent === "00000") {
-        clearDisplay();
-        isDisplayPopulated = false;
-    }
-    display.textContent += text;
-}
-
-function removeSingleCharacter() {
-    display.textContent = display.textContent.slice(
-        0,
-        display.textContent.length - 1
-    );
-}
-
-numBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        populateDisplay(e.target.getAttribute("data-key"));
-    });
-});
-
-operatorBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        populateDisplay(` ${e.target.getAttribute("data-key")}  `);
-    });
-});
-
-resultBtn.addEventListener("click", () => {
-    const input = parseInput(display.textContent);
-    clearDisplay();
-    if (input.length < 3) {
-        populateDisplay("INVALID OPERATION");
+function calculateEquation() {
+    const input = parseInput(eqnDisplay.textContent);
+    // console.log(input);
+    clearResultDisplay();
+    if (input.length === 1) {
+        if (["*", "+", "-", "÷"].includes(input[0])) {
+            populateDisplay("INVALID OPERATION", resultDisplay);
+        } else populateDisplay(input, resultDisplay);
+    } else if (input.length === 2 || input[0] === "*" || input[0] === "÷") {
+        clearEquationDisplay();
+        populateDisplay("INVALID OPERATION", resultDisplay);
     } else {
         let i = 0;
         let j = 2;
         let operator = 1;
         let result;
-        while (j < input.length) {
-            let firstOperand = result === undefined ? input[i] : result;
-            result = operate(
-                parseFloat(firstOperand),
-                parseFloat(input[j]),
-                input[operator].trim()
-            );
-            j = j + 2;
-            operator = operator + 2;
+        let firstOperand;
+        try {
+            if (input[0] === "") input.splice(0, 1, "0"); // array is prepended with 0 when the user enter a number like -2 or +2 at the start (which is a valid syntax)
             // console.log(input);
-            console.log(result);
+
+            while (j < input.length) {
+                firstOperand = result === undefined ? input[i] : result;
+                result = operate(
+                    parseFloat(firstOperand),
+                    parseFloat(input[j]),
+                    input[operator].trim()
+                );
+                j = j + 2;
+                operator = operator + 2;
+            }
+            if (result) {
+                populateDisplay(result, resultDisplay);
+            } else {
+                populateDisplay("INVALID OPERATION", resultDisplay);
+            }
+        } catch (error) {
+            console.log(error);
+            populateDisplay("INVALID OPERATION", resultDisplay);
         }
-        populateDisplay(result);
     }
-    isDisplayPopulated = true;
+}
+
+function clearResultDisplay() {
+    resultDisplay.textContent = "";
+}
+
+function clearEquationDisplay() {
+    eqnDisplay.textContent = "_";
+}
+
+function clearEverything() {
+    eqnDisplay.textContent = "_";
+    resultDisplay.textContent = "00000";
+}
+
+function populateDisplay(text, displayPosition) {
+    if (displayPosition.textContent === "00000") {
+        clearResultDisplay();
+    }
+    if (text === "INVALID OPERATION") clearEquationDisplay();
+    displayPosition.textContent += text;
+}
+
+function removeSingleCharacter() {
+    eqnDisplay.textContent = eqnDisplay.textContent.slice(
+        0,
+        eqnDisplay.textContent.length - 1
+    );
+}
+
+numBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (eqnDisplay.textContent === "_") eqnDisplay.textContent = "";
+        populateDisplay(e.target.getAttribute("data-key"), eqnDisplay);
+    });
 });
-clearBtn.addEventListener("click", clearDisplay);
-clearEntryBtn.addEventListener("click", clearEntry);
+
+decimalBtn.addEventListener("click", (e) => {
+    if (eqnDisplay.textContent === "_") eqnDisplay.textContent = "";
+    populateDisplay(".", eqnDisplay);
+    e.target.disabled = true;
+});
+
+operatorBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (eqnDisplay.textContent === "_") eqnDisplay.textContent = "";
+        populateDisplay(` ${e.target.getAttribute("data-key")}  `, eqnDisplay);
+        decimalBtn.disabled = false;
+    });
+});
+
+resultBtn.addEventListener("click", () => {
+    calculateEquation();
+    if (decimalBtn.disabled) {
+        decimalBtn.disabled = false;
+    }
+});
+
+clearBtn.addEventListener("click", clearEverything);
+clearEntryBtn.addEventListener("click", clearEquationDisplay);
 backspaceBtn.addEventListener("click", () => {
-    if (display.textContent.length > 0) removeSingleCharacter();
+    if (eqnDisplay.textContent.length > 0) removeSingleCharacter();
 });
